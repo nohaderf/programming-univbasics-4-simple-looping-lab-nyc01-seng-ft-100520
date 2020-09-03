4ed689e8ad3d05581ea29a2ec931d420d7147198
Freda Hon <freda.hon@gmail.com>
2020-09-03 23:45:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,9 +25,10 @@ def output_array(array)
 end
 
 def return_string_array(array)
+  new_array = [ ]
   counter = 0
   while counter < array.length
-    array[counter].to_s
+    new_array.push(array[counter].to_s)
     counter += 1
   end
 end
