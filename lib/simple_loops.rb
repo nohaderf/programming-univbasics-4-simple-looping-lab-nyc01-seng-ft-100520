c65a8ce965593e8dc30aa389b23349a43ef5635d
Freda Hon <freda.hon@gmail.com>
2020-09-03 23:42:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,7 @@
 
 def loop_message_five_times(message)
   counter = 0
-  while counter < 5 do
+  while counter < 5
     puts message
     counter += 1
   end
@@ -10,9 +10,10 @@ end
 
 def loop_message_n_times(message, n)
   counter = 0
-  while counter < n
+  while counter < n 
     puts message
     counter += 1
+  end
 end
 
 def output_array(array)
